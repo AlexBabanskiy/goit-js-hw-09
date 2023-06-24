ada9c811849860a90b7b6d6e803f48dfdf6db2c7
AlexBabanskiy <124041034+AlexBabanskiy@users.noreply.github.com>
2023-06-24 15:49:16 +0200

    Version1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+let timerId = null;
+
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+
+buttonStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+buttonStop.addEventListener('click', () => {
+  clearInterval(timerId);
+});
