02c4e83de1f2747241467a36a0fcc3ad2efc1959
AlexBabanskiy <124041034+AlexBabanskiy@users.noreply.github.com>
2023-06-24 19:38:52 +0200

    Version1.3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,5 @@
+import Notiflix from 'notiflix';
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
@@ -10,10 +12,15 @@ const buttonStart = document.querySelector('[data-start]');
 const buttonStop = document.querySelector('[data-stop]');
 
 buttonStart.addEventListener('click', () => {
+  buttonStart.setAttribute('disabled', '');
+  Notiflix.Notify.success('Починаємо кольорову какафонію!');
   timerId = setInterval(() => {
     document.body.style.backgroundColor = getRandomHexColor();
   }, 1000);
 });
 buttonStop.addEventListener('click', () => {
+  Notiflix.Notify.warning('Зупиняємо цей кошмар!');
+  buttonStart.removeAttribute('disabled', '');
+
   clearInterval(timerId);
 });
